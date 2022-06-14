7859fe055ddec566f6d3c8b1d37edfb844f51cd6
hlhcode <1730440896@qq.com>
2022-06-14 18:20:05 +0800

    修改email

--- Comm/Email.py
+++ Comm/Email.py
@@ -1,8 +1,4 @@
-# -*- coding = uft-8 -*-
-# @Time : 2022/6/13 22:35
-# @Author : »ÆÁ¢»Û
-# @File : Email.py
-# @Software : PyCharm
+# -*- coding: utf-8 -*-
 import smtplib
 import os
 import logging
@@ -12,8 +8,8 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 from Conf.config import smtp_cfg, email_cfg
 
-FILESIZE = 20  # µ¥Î»M£¬µ¥¸ö¸½¼ş´óĞ¡
-FILECOUNT = 10  # ¸½¼ş¸öÊı
+FILESIZE = 20  # å•ä½Mï¼Œå•ä¸ªé™„ä»¶å¤§å°
+FILECOUNT = 10  # é™„ä»¶ä¸ªæ•°
 smtp_cfg = smtp_cfg
 email_cfg = email_cfg
 logger = logging.getLogger("main.email")
@@ -29,52 +25,52 @@ class Email:
 
     def _message_init(self):
         if self.subject:
-            self.message['subject'] = Header(self.subject,'utf-8')  #ÓÊ¼ş±êÌâ
+            self.message['subject'] = Header(self.subject, 'utf-8')  # é‚®ä»¶æ ‡é¢˜
         else:
             raise ValueError("Invalid subject")
 
         self.message['from'] = email_cfg['sender']  # from
-        self.message['to'] = email_cfg['receivers'] # to
+        self.message['to'] = email_cfg['receivers']  # to
 
         if self.context:
-            self.message.attach(MIMEText(self.context,'html','utf-8'))  #ÓÊ¼şÕıÎÄÄÚÈİ
-        # ÓÊ¼ş¸½¼ş
+            self.message.attach(MIMEText(self.context, 'html', 'utf-8'))  # é‚®ä»¶æ­£æ–‡å†…å®¹
+        # é‚®ä»¶é™„ä»¶
         if self.attachment:
-            if isinstance(self.attachment,str):
+            if isinstance(self.attachment, str):
                 self._attach(self.attachment)
-            if isinstance(self.attachment,list):
+            if isinstance(self.attachment, list):
                 count = 0
                 for each in self.attachment:
                     if count <= FILECOUNT:
                         self._attach(each)
-                        count +=1
+                        count += 1
                     else:
-                        logger.warning('Attachments is more than ',FILECOUNT)
+                        logger.warning('Attachments is more than ', FILECOUNT)
                         break
 
     def _attach(self, file):
-        if os.path.isfile(file) and os.path.getsize(file)<=FILESIZE*1024*1024:
-            attach = MIMEApplication(open(file,'rb').read())
-            attach.add_header('Content-Disposition','attachment',filename=os.path.basenae(file))
-            attach['Content-Type']= 'application/octet-stream'
+        if os.path.isfile(file) and os.path.getsize(file) <= FILESIZE * 1024 * 1024:
+            attach = MIMEApplication(open(file, 'rb').read())
+            attach.add_header('Content-Disposition', 'attachment', filename=os.path.basenae(file))
+            attach['Content-Type'] = 'application/octet-stream'
             self.message.attach(attach)
         else:
-            logger.error('The attachment is not exist or more than %sM: %s'%(FILESIZE,file))
+            logger.error('The attachment is not exist or more than %sM: %s' % (FILESIZE, file))
 
     def send_email(self):
-        s = smtplib.SMTP_SSL(smtp_cfg['host'],int(smtp_cfg['port']))
+        s = smtplib.SMTP_SSL(smtp_cfg['host'], int(smtp_cfg['port']))
         result = True
         try:
-            s.login(self.smtp_cfg['user'],self.smtp_cfg['passwd'])
-            s.sendmail(self.smtp_cfg['sender'],self.smtp_cfg['receivers'],self.message.as_string())
+            s.login(self.smtp_cfg['user'], self.smtp_cfg['passwd'])
+            s.sendmail(self.smtp_cfg['sender'], self.smtp_cfg['receivers'], self.message.as_string())
         except smtplib.SMTPException as e:
             result = False
-            logger.error('Send mail faild',exc_info=True)
+            logger.error('Send mail faild', exc_info=True)
         finally:
             s.close()
         return result
 
 
-mail = Email('title' ,'context','WBS_1219063746359296_1654765657044.xlsx')
+mail = Email('title', 'context', 'WBS_1219063746359296_1654765657044.xlsx')
 send = mail.send_email()
 print(send)
